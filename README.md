ed5f548fb43cfa3b57f238309d837f8c9360540d
Kazuo Moriwaka <moriwaka@gmail.com>
2022-03-19 00:05:43 +0900

    fill in readme, remove debug flag

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # pdfwav2mp4
 generate mp4 from a pdf and wav files for each pages 
+
+Usage:
+
+```
+$ pdfwav2mp4 'My Presentation.pdf' 'A directory which contains wav'
+```
+will create 'My Presentation.mp4'
+
+Dependency:
+
+- ffmpeg
+- GraphicsMagick
+
+This script was forked from https://github.com/automating-presentations/slide2mp4
---
+++ pdfwav2mp4.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#
+# Copyright (C) 2022 Kazuo Moriwaka
+# Copyright (C) 2021 Hirofumi Kojima
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+DENSITY=600
+GEOMETRY=1920x1080
+FPS=30
+
+ffmpeg_loglevel=warning
+
+print_usage ()
+{
+	echo "Usage:"
+	echo "	$(basename $0) PDF_FILE WAV_DIR" 
+	exit
+}
+
+PDF_FILE=$1
+shift || print_usage
+WAV_DIR=$1
+shift || print_usage
+shift && print_usage
+
+mkdir -p tmp
+
+if [ ! -e tmp/0000.png -o "$PDF_FILE" -nt tmp/0000.png ]; then
+  rm tmp/*.png
+  gm convert -density $DENSITY -geometry $GEOMETRY +adjoin "$PDF_FILE" png:tmp/%04d.png
+fi
+
+pngs=(tmp/*png)
+wavs=("$WAV_DIR"/*wav)
+
+if [ ${#pngs[@]} != ${#wavs[@]} ]; then
+  echo "Error: PDF_FILE pages(${#pngs[@]} isn't same with WAV_DIR *.wav(${#wavs[@]})"
+  exit
+fi
+
+for i in ${!wavs[@]}; do
+    wav=${wavs[$i]} 
+    png=${pngs[$i]}
+    mp4=${png%.png}.mp4
+    if [ ! -e $mp4 -o "$wav" -nt $mp4 -o $png -nt $mp4 ]; then
+      ffmpeg -loglevel $ffmpeg_loglevel -y -loop 1 -i $png -i "$wav" \
+      -acodec aac -vcodec libx264 -tune stillimage -pix_fmt yuv420p -shortest -r $FPS $mp4
+      touch $mp4
+    fi
+done
+
+
+rm -f tmp/list.txt 
+mp4s=(tmp/*mp4)
+for mp4 in ${mp4s[@]}; do echo "file ${mp4#tmp/}" >> tmp/list.txt; done
+ffmpeg -loglevel $ffmpeg_loglevel -y -f concat -i tmp/list.txt -c copy "${PDF_FILE%.pdf}.mp4"
+
